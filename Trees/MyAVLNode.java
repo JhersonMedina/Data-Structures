22aedbf2145a2213eec457f812ec3038f38dae20
JhersonMedina <admedinaco@unal.edu.co>
2018-10-26 11:31:38 -0500

    Adding AVl Nodes

---
+++ Trees/MyAVLNode.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bst;
+
+/**
+ *
+ * @author Jhers
+ * @Param <T>
+ */
+public class MyAVLNode<T> {
+    private T element;
+    private MyAVLNode<T> left;
+    private MyAVLNode<T> right;
+    private int height;
+
+    public MyAVLNode(T element, MyAVLNode<T> left, MyAVLNode<T> right) {
+        this.element = element;
+        this.left = left;
+        this.right = right;
+        height = 0;
+    }
+
+    public T getElement() {
+        return element;
+    }
+
+    public void setElement(T element) {
+        this.element = element;
+    }
+
+    public MyAVLNode<T> getLeft() {
+        return left;
+    }
+
+    public void setLeft(MyAVLNode<T> left) {
+        this.left = left;
+    }
+
+    public MyAVLNode<T> getRight() {
+        return right;
+    }
+
+    public void setRight(MyAVLNode<T> right) {
+        this.right = right;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    
+    
+    
+    
+}
