c0e8788ef3d45c51f0d75d76d7e51101faa6ec6e
JhersonMedina <admedinaco@unal.edu.co>
2018-10-26 11:29:49 -0500

    Changing main to apply AVL Tres

--- Trees/Main.java
+++ Trees/Main.java
@@ -19,15 +19,15 @@ public class Main {
         //Binary seatch tree
         System.out.println("BINARY SEARH TREE: ");
         MyBST<Integer> myTree = new MyBST<>();
-        myTree.height();
-        myTree.insert(7);
+        myTree.insert(6);
         myTree.insert(5);
         myTree.insert(2);
         myTree.insert(10);
         myTree.insert(8);
-        myTree.insert(9);
+        myTree.insert(7);
 
         System.out.println("Tree itself:");
+        System.out.println("Tree height: " + myTree.height());
         myTree.print();
 
         System.out.println("Smallest element: " + myTree.findMin());
@@ -38,23 +38,37 @@ public class Main {
 
         System.out.println("is 5 conatained? : " + myTree.contains(5));
         System.out.println("is 0 contained: " + myTree.contains(0));
-
+        
+        System.out.println("Tree after removing element:");
         System.out.println("Tree height: " + myTree.height());
+        myTree.print();
 
         //AVL Tree
         System.out.println("AVL TREE: ");
         MyAVLT<Integer> myAVLTree = new MyAVLT<>();
-        myAVLTree.height();
 
+        myAVLTree.insert(6);
+        myAVLTree.insert(5);
         myAVLTree.insert(2);
-       // myAVLTree.insert(1);
-        //myAVLTree.insert(4);
-        //myAVLTree.insert(3);
-        //myAVLTree.insert(5);
-        //myAVLTree.insert(6);
-        
-        //myAVLTree.print();
-        //System.out.println("AVL Tree: " + myAVLTree.height());
+        myAVLTree.insert(10);
+        myAVLTree.insert(8);
+        myAVLTree.insert(7);
 
+        System.out.println("Tree itself:");
+        myAVLTree.print();
+
+        System.out.println("Smallest element: " + myAVLTree.findMin());
+        System.out.println("Biggest element: " + myAVLTree.findMax());
+
+        myAVLTree.remove(2);
+        System.out.println("Smallest Element: " + myAVLTree.findMin());
+
+        System.out.println("is 5 conatained? : " + myAVLTree.contains(5));
+        System.out.println("is 0 contained: " + myAVLTree.contains(0));
+
+        System.out.println("Tree after removing element:");
+        myAVLTree.print();
+
+        //System.out.println("AVL Tree: " + myAVLTree.height());
     }
 }
